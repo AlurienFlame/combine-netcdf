2dd11977e643fb1cb25f1e0583fcbc8c63d36973
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2025-11-05 14:39:23 -0800

    reading data from netcdf file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+use rocket::response::status;
+
 #[macro_use] extern crate rocket;
 
-#[get("/part_a?<name>")]
-fn part_a(name: &str) -> &'static str {
-    "Hello, world!"
+#[post("/part_a?<name>", format = "application/x-netcdf", data = "<input>")]
+fn part_a(name: &str, input: &[u8]) -> status::Accepted<String> {
+    let file = netcdf::open_mem(Some(name), input).expect("fail");
+    let var = &file.variable("Hexahedron_Vertex").expect("Could not find variable 'data'");
+    let data_i32 = var.get_value::<i32, _>([1, 1]).expect("fail");
+    status::Accepted(format!("id: '{}'", data_i32))
 }
 
-#[get("/part_b?<name>")]
-fn part_b(name: &str) -> &'static str {
+#[post("/part_b?<name>", format = "application/x-netcdf", data = "<input>")]
+fn part_b(name: &str, input: Vec<u8>) -> &'static str {
     "Hello, world!"
 }
 
