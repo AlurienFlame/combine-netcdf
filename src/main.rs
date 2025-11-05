eb4e0d1b20e093d648436af98910787f4b01d578
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2025-11-05 12:42:44 -0800

    establish routes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
+#[get("/part_a?<name>")]
+fn part_a(name: &str) -> &'static str {
     "Hello, world!"
+}
+
+#[get("/part_b?<name>")]
+fn part_b(name: &str) -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/read?<name>")]
+fn read(name: &str) -> &str {
+    return name;
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![part_a, part_b, read])
 }
